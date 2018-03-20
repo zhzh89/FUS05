2eee7ca0955c11c7057b8b52036eb512329e0904
zhzh89 <mijeong0733@gmail.com>
2018-03-20 17:21:14 +0900

    모바일 메뉴 제어를 위한 버튼 완성

--- css/js/example-12.js
+++ css/js/example-12.js
@@ -1,8 +1,18 @@
 $(document).ready(function(){
     // console.log('ok!');
     var menubar = $('.menubar, .btn-menubar');
+    var bar = $('.menubar');
+    var btn = $('.btn-menubar');
     var menu = $('.menu');
+    
     menubar.on('click',function(){
         menu.toggleClass('menu-act');
+        bar.toggleClass('menubar-act');
+        if(menu.hasClass('menu-act')){
+            btn.text('메뉴 닫기');
+
+        }else{
+            btn.text('메뉴 열기');
+        }
     });
 });
